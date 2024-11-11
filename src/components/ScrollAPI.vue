d85934eb41a7bb0cf651d6c65e5106d8f16e9c3b
MrArchduke <darkvader126567@gmail.com>
2024-11-11 16:11:34 -0500

    Commit I

---
+++ src/components/ScrollAPI.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  mounted() {
+    this.$refs.scrollableContainer.addEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll() {
+      // Perform actions based on scroll position
+      console.log(this.$refs.scrollableContainer.scrollTop);
+    },
+  },
+};
+</script>
+<template>
+  <div ref="scrollableContainer">
+    <!-- Your content here -->
+  </div>
+</template>
